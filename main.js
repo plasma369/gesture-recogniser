e988b7fc58b9d6568a443797776d0ab8c8664d85
plasma369 <76848965+plasma369@users.noreply.github.com>
2021-06-14 16:39:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0;
+
+diffrence=0;
+rightWristX=0;
+leftWristX=0;
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("model Loaded");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + ", noseY = " +noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        diffrence = leftWristX - rightWristX;
+    }
+}
+
+function draw()
+{
+    background('#F79191');
+    fill('#4287f5');
+    stroke('#245cb5');
+    square(noseX, noseY, diffrence);
+}
